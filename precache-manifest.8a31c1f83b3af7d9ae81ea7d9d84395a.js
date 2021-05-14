e83fa9b3331422ea0242ae5b64bc877144d7594f
HelyeFab <emmanuelfabiani23@gmail.com>
2021-05-14 13:13:36 +0100

    first commit

---
+++ precache-manifest.8a31c1f83b3af7d9ae81ea7d9d84395a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14557ec7269592d5fab9",
+    "url": "/static/css/main.8374a540.chunk.css"
+  },
+  {
+    "revision": "14557ec7269592d5fab9",
+    "url": "/static/js/main.14557ec7.chunk.js"
+  },
+  {
+    "revision": "82fb7def2a2d90ba7416",
+    "url": "/static/js/1.82fb7def.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b914d41e02911c735921ed5334f48676",
+    "url": "/index.html"
+  }
+];
